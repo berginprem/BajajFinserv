4b291b822bd80feb1bc77336e0c13cd79b7e06a6
berginprem <76146666+berginprem@users.noreply.github.com>
2023-05-19 18:21:15 +0530

    sfdsfs

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get('/asd', (req, res) => {
+    res.send('MEOW!!')
+})
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000")
+})
