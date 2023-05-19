f4a9e313833efe8382820c2d2a9c242173b7c47f
berginprem <76146666+berginprem@users.noreply.github.com>
2023-05-19 21:47:31 +0530

    multer

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+
 const app = express();
 app.use(express.urlencoded({ extended: true }))
+const multer = require('multer');
+const upload = multer();
+app.use(upload.any());
 app.use(express.json())
 
 
